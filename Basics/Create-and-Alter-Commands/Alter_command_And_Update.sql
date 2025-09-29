5467d4fecdca226add0eedad48eefad6074e5e6c
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-09-29 16:31:18 +0530

    Create and Alter

---
+++ Basics/Create-and-Alter-Commands/Alter_command_And_Update.sql
@@ -0,0 +1,15 @@
+ALTER TABLE Student
+ADD Department VARCHAR(50);
+
+-- Update Department values
+UPDATE Student
+SET Department = 'Computer Science'
+WHERE StudentID = 1;
+
+UPDATE Student
+SET Department = 'Electronics'
+WHERE StudentID = 2;
+
+UPDATE Student
+SET Department = 'Mechanical'
+WHERE StudentID = 3;
