25c408e1fd6cb7878786df46e3bc35576e131751
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-09-29 16:32:23 +0530

    Drop and Truncate

---
+++ Basics/Drop-and-Truncate/Information_about_Drop_and_Truncate.sql
@@ -0,0 +1,20 @@
+-- 1. DROP Table
+-- To delete an entire table including its data and structure:
+
+-- Syntax:
+-- DROP TABLE table_name;
+
+-- 2. DROP database
+-- To delete an entire database and all of its associated tables:
+
+-- Syntax:
+-- DROP DATABASE database_name;
+
+-- SQL TRUNCATE Command
+-- The TRUNCATE command is a Data Definition Language (DDL) action that removes all rows from a table but preserves the structure of the table for future use.
+
+-- Syntax:
+-- TRUNCATE TABLE  table_name;
+
+
+
