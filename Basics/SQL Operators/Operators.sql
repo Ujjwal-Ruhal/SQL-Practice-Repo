3653251a369dc5102b293f90031ea9a79bb9de76
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-09-30 19:17:53 +0530

    commit

---
+++ Basics/SQL Operators/Operators.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE Operation;
+USE Operation;
+
+CREATE TABLE MATHS(
+ROLL_NUMBER INT,
+S_NAME VARCHAR(10),
+MARKS INT);
+
+INSERT INTO MATHS (id, name, marks) VALUES
+(1, 'ABHI', 70),
+(2, 'RAVI', 80),
+(3, 'ARJUN', 90),
+(4, 'SAM', 100),
+(5, 'MOHAN', 50),
+(6, 'ROHAN', 10),
+(7, 'ROCKY', 20),
+(8, 'AYUSH', 40),
+(9, 'NEHA', 30),
+(10, 'KRITI', 60);
+
+SELECT * FROM MATHS;
