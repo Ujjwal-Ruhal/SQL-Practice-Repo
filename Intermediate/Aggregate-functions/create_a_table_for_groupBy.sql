692fbc1b780b8fa4a47d8fac9614b36c44a7bd03
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-02 22:54:48 +0530

    create a table for GROUP-BY

---
+++ Intermediate/Aggregate-functions/create_a_table_for_groupBy.sql
@@ -0,0 +1,12 @@
+CREATE TABLE student (
+  name VARCHAR(50),
+  year INT,
+  subject VARCHAR(50) );
+
+INSERT INTO student (name, year, subject) VALUES
+('Avery', 1, 'Mathematics'),
+('Elijah', 2, 'English'),
+('Harper', 3, 'Science'),
+('James', 1, 'Mathematics'),
+('Charlotte', 2, 'English'),
+('Benjamin', 3, 'Science');
