2d5d8410d0dbffd25836fafd57cfa1fed4ebe8fb
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-05 23:28:40 +0530

    Create the Authors Table

---
+++ Intermediate/Basic JOINs/FULL  JOIN/Create the Authors Table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Authors (
+    AUTHOR_ID INT PRIMARY KEY,
+    AUTHOR_NAME VARCHAR(100)
+);
+
+-- Insert Data into Authors Table
+INSERT INTO Authors (AUTHOR_ID, AUTHOR_NAME) VALUES
+(1, 'Ram Kumar'),
+(2, 'Shyam Sunder'),
+(3, 'Sita Singh'),
+(4, 'Mohan Gupta'),
+(5, 'Raj Kapoor');
