d415da9327c30936f6e5dd96e35abb07687ca7e0
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-05 23:29:10 +0530

    Create the Publishers Table

---
+++ Intermediate/Basic JOINs/FULL  JOIN/Create the Publishers Table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Publishers (
+    PUBLISHER_ID INT PRIMARY KEY,
+    PUBLISHER_NAME VARCHAR(100)
+);
+
+-- Insert Data into Publishers Table
+INSERT INTO Publishers (PUBLISHER_ID, PUBLISHER_NAME) VALUES
+(1, 'Pearson'),
+(2, 'Wiley'),
+(3, 'McGraw-Hill'),
+(4, 'Oxford'),
+(5, 'Cambridge');
