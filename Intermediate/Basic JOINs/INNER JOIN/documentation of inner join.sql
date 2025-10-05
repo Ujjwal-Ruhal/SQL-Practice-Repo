75231a09debfc3854a259782a17121b5c9a97bda
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-05 23:05:38 +0530

    documentation of inner join

---
+++ Intermediate/Basic JOINs/INNER JOIN/documentation of inner join.sql
@@ -0,0 +1,14 @@
+/*
+SQL Inner Join
+INNER JOIN is used to combine rows from two or more tables based on a related column. It returns only the rows that have matching values in both tables,
+filtering out non-matching records. It is commonly used in relational databases and useful for working with related data.
+*/
+
+/*
+Syntax:
+
+SELECT columns FROM table1
+INNER JOIN table2
+ON table1.column_name = table2.column_name;
+*/
+
