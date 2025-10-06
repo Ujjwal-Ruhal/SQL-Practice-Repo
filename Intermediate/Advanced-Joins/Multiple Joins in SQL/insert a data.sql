4652bba5067504540fb1cb6fcc6a17030116263f
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-06 23:24:05 +0530

    insert data into tables

---
+++ Intermediate/Advanced-Joins/Multiple Joins in SQL/insert a data.sql
@@ -0,0 +1,22 @@
+-- Step 2: Insert Data into Tables
+
+-- students
+INSERT INTO students VALUES
+(1,'Liam','CSE'),
+(2,'Emma','ECE'),
+(3,'Noah','ECE'),
+(4,'Olivia','CSE');
+
+-- marks
+INSERT INTO marks VALUES
+(1,95),
+(2,85),
+(3,80),
+(4,65);
+
+-- attendance
+INSERT INTO attendance VALUES
+(1,75),
+(2,65),
+(3,80),
+(4,87);
