34a86dd481cbc48a1a8859ba7fd114d2051233bb
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-06 23:20:41 +0530

    documentation of Multiple JOIN

---
+++ Intermediate/Advanced-Joins/Multiple Joins in SQL/documentation of Multiple Joins.sql
@@ -0,0 +1,6 @@
+/*
+Multiple Joins in SQL
+In SQL, multiple joins allow you to combine data from more than one table in a single query. 
+This helps efficiently retrieve complex datasets without running multiple separate queries.
+*/
+-- For example, we will combine data from a students, marks and attendance table to get marks and attendance of students with specific conditions.
