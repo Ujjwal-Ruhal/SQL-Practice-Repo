554a5d9cb5f0056ec00c7044b1972309a57f6a96
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-06 23:22:44 +0530

    create a database

--- Intermediate/Advanced-Joins/Multiple Joins in SQL/create a database and tables.sql
+++ Intermediate/Advanced-Joins/Multiple Joins in SQL/create a database and tables.sql
@@ -2,3 +2,9 @@
 Step 1: Set up the Database and Tables
 We will first create a database called geeks that contains three tables: students, marks and attendance and then run our queries on those tables.
 */
+CREATE DATABASE hello;
+USE hello;
+
+CREATE TABLE students(id INT, name VARCHAR(50), branch VARCHAR(50));
+CREATE TABLE marks(id INT, marks INT);
+CREATE TABLE attendance(id INT, attendance INT);
