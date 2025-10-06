e10a26d708ee5a796d330d5d172e8bfa89b46e04
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-06 23:03:57 +0530

    create a table

---
+++ Intermediate/Advanced-Joins/create a table.sql
@@ -0,0 +1,7 @@
+/*
+Example: Employees and Their Managers
+We have a table GFGemployees with employee_id, employee_name and manager_id. Each employee is linked to their manager using manager_id. Our goal is to extract employees along with their respective managers’ names.
+
+Here is the query to create the table and insert sample data:
+*/
+
