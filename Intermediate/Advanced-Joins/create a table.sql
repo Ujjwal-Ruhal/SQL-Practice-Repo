1e42e85a0b863e0c596b35d263a7433027795c5e
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-06 23:05:45 +0530

    add a query

--- Intermediate/Advanced-Joins/create a table.sql
+++ Intermediate/Advanced-Joins/create a table.sql
@@ -16,3 +16,9 @@ VALUES  (1, 'Zaid', 3),
                (3, 'Raman', 4),  
                (4, 'Kamran', NULL),  
                (5, 'Farhan', 4);
+               
+/*
+To retrieve the list of employees with their corresponding managers, we perform a self join on the GFGemployees table. We use two aliases: e for employees and m for managers.
+
+By joining manager_id from the employee side with employee_id from the manager side, we establish the relationship between employees and their managers.
+*/
