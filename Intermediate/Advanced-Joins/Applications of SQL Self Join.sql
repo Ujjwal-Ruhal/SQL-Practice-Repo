d0cca13bdb6bfae590e3b475c4aecb5a5c1f82a6
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-06 23:07:20 +0530

    Applications of SQL Self Join

---
+++ Intermediate/Advanced-Joins/Applications of SQL Self Join.sql
@@ -0,0 +1,10 @@
+/*
+Applications of SQL Self Join
+SQL Self Join is widely used in different scenarios, such as:
+
+Hierarchical Data: Representing organizational structures like employee–manager, category–subcategory or parent–child relationships.
+Finding Relationships: Identifying relationships within the same table, for example linking friends in a social network or mapping task dependencies.
+Data Comparison: Comparing rows within same table, such as salaries of employees in same department.
+Detecting Duplicates: Finding duplicate records in a table by joining it with itself on duplicate criteria.
+Sequential Data Analysis: Comparing current rows with previous or next rows, useful for analyzing trends in sales or time-series data.
+*/
