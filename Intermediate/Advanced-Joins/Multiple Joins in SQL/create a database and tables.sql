3d25ecc66222a4c8d900b0e8ab9285b094d1876f
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-06 23:21:52 +0530

    create a database and tables

---
+++ Intermediate/Advanced-Joins/Multiple Joins in SQL/create a database and tables.sql
@@ -0,0 +1,4 @@
+/*
+Step 1: Set up the Database and Tables
+We will first create a database called geeks that contains three tables: students, marks and attendance and then run our queries on those tables.
+*/
