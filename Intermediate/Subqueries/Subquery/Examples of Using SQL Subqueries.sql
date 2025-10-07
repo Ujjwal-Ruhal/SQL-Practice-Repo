950fdf9a282625ad38fb76a78212271c8f6d7298
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-07 22:42:21 +0530

    Examples of Using SQL Subqueries

---
+++ Intermediate/Subqueries/Subquery/Examples of Using SQL Subqueries.sql
@@ -0,0 +1,6 @@
+/*
+Examples of Using SQL Subqueries
+These examples showcase how subqueries can be used for various operations like selecting, updating, deleting or inserting data, providing insights into their syntax and functionality. Through these examples, we will understand flexibility and importance of subqueries in simplifying complex database tasks.
+
+Consider the following two tables:
+*/
