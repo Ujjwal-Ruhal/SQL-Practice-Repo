7b0807ff633c008822f847d2abd2d31f42d72fc7
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-07 22:35:43 +0530

    syntax

--- Intermediate/Subqueries/Subquery/Documentation of Subquery.sql
+++ Intermediate/Subqueries/Subquery/Documentation of Subquery.sql
@@ -9,3 +9,13 @@ Aggregating: applying functions like SUM, COUNT, AVG with dynamic conditions.
 Updating: modifying data using values from other tables.
 Deleting: removing rows based on criteria from another query.
 */
+/*
+While there is no universal syntax for subqueries, they are commonly used in SELECT statements as follows.
+
+Syntax:
+
+SELECT column_name
+FROM table_name
+WHERE column_name expression operator 
+   (SELECT column_name FROM table_name WHERE ...);
+*/
