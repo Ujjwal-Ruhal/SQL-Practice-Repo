757d1195775cc8915a5fe6bd30446ce0410a550c
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-07 22:34:59 +0530

    update file

--- Intermediate/Subqueries/Subquery/Documentation of Subquery.sql
+++ Intermediate/Subqueries/Subquery/Documentation of Subquery.sql
@@ -3,3 +3,9 @@ SQL Subquery
 A subquery in SQL is a query nested inside another SQL query. It allows complex filtering, aggregation and data manipulation by using the result of one query inside another. 
 They are an essential tool when we need to perform operations like:
 */
+/*
+Filtering: selecting rows based on conditions from another query.
+Aggregating: applying functions like SUM, COUNT, AVG with dynamic conditions.
+Updating: modifying data using values from other tables.
+Deleting: removing rows based on criteria from another query.
+*/
