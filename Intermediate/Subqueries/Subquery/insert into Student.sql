71266d872063504e16551ea02486c3d8dcea660a
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-07 22:48:57 +0530

    Insert data

---
+++ Intermediate/Subqueries/Subquery/insert into Student.sql
@@ -0,0 +1,6 @@
+INSERT INTO Student (NAME, ROLL_NO, LOCATION, PHONE_NUMBER) VALUES
+('Ram', 101, 'Chennai', '9988775566'),
+('Raj', 102, 'Coimbatore', '8877665544'),
+('Sasi', 103, 'Madurai', '7766553344'),
+('Ravi', 104, 'Salem', '8989898989'),
+('Sumathi', 105, 'Kanchipuram', '8989856868');
