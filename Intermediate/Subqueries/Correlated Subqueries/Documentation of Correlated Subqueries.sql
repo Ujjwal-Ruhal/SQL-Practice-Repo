75159c6d0571c55eefea552beddc232d376605af
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-07 22:58:08 +0530

    Documentation of Correlated Subqueries

---
+++ Intermediate/Subqueries/Correlated Subqueries/Documentation of Correlated Subqueries.sql
@@ -0,0 +1,9 @@
+/*
+SQL Correlated Subqueries
+A correlated subquery is a subquery that depends on values from the outer query. Unlike a regular (non-correlated) subquery, it is evaluated once for every row in the outer query. This makes correlated subqueries dynamic and highly useful for solving complex database problems like row-by-row comparisons, filtering, and conditional updates.
+
+Executes once for each row of the outer query.
+Subquery uses values from the outer query.
+Ideal for ranking, row-specific calculations, and conditional logic
+*/
+
