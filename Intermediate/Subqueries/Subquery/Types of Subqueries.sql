0de72e07c46774cec68bb409c75a8402aa111af0
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-07 22:40:02 +0530

    Example 2

--- Intermediate/Subqueries/Subquery/Types of Subqueries.sql
+++ Intermediate/Subqueries/Subquery/Types of Subqueries.sql
@@ -9,3 +9,14 @@ SELECT * FROM Employees
 WHERE Salary = (SELECT MAX(Salary) FROM Employees);
 
 -- Output: Returns the employee(s) with the highest salary.
+
+/*
+2. Multi-Row Subquery
+Returns multiple rows as the result.
+Requires operators that can handle multiple values, such as IN, ANY or ALL
+*/
+-- Example:
+SELECT * FROM Employees
+WHERE DepartmentID IN (SELECT DepartmentID FROM Departments WHERE Location = 'New York');
+
+-- Output: Fetches employees working in all New York departments.
