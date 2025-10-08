7a144a0044c831a94e34001849ff4fc4ce8fcc14
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-08 23:21:07 +0530

    updated

--- Intermediate/Functions/String functions.sql
+++ Intermediate/Functions/String functions.sql
@@ -2,3 +2,8 @@
 /*
 SQL String Functions are powerful tools that allow us to manipulate, format, and extract specific parts of text data in our database. These functions are essential for tasks like cleaning up data, comparing strings, and combining text fields. Whether we are working with names, addresses, or any form of textual data, mastering SQL string functions is crucial for efficient data handling and analysis.
 */
+
+/*
+Common SQL String Functions
+String functions are used to perform an operation on input string and return an output string. Below are some of the most commonly used SQL string functions:
+*/
