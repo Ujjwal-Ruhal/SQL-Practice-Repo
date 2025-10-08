795b5700e6907816a532fddbd568251b7e3cb020
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-08 23:13:29 +0530

    Categories of SQL Functions

---
+++ Intermediate/Functions/Categories of SQL Functions.sql
@@ -0,0 +1,4 @@
+/*
+Categories of SQL Functions
+SQL functions are powerful tools that help streamline queries, perform operations, and manipulate data efficiently. They are essential for handling various tasks within SQL queries and database management.
+*/
