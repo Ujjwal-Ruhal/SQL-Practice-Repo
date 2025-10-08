bea3a7da0c32cd7a8f8ee1d28e3b16ba4231c271
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-08 23:20:37 +0530

    add on String functions

---
+++ Intermediate/Functions/String functions.sql
@@ -0,0 +1,4 @@
+-- String functions
+/*
+SQL String Functions are powerful tools that allow us to manipulate, format, and extract specific parts of text data in our database. These functions are essential for tasks like cleaning up data, comparing strings, and combining text fields. Whether we are working with names, addresses, or any form of textual data, mastering SQL string functions is crucial for efficient data handling and analysis.
+*/
