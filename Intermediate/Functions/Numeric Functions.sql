b219ad21258d3d06a7d5d326339b47f1fab4b3c1
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-09 22:56:57 +0530

    Numeric Functions

---
+++ Intermediate/Functions/Numeric Functions.sql
@@ -0,0 +1,10 @@
+/*
+SQL | Numeric Functions
+SQL Numeric Functions are built-in tools that allow you to perform mathematical and arithmetic operations on numeric data. They are widely used in financial, statistical, and reporting tasks to simplify calculations.
+
+Operate on numeric data types like INT, FLOAT, DECIMAL, DOUBLE.
+Perform basic arithmetic operations (addition, subtraction, multiplication, division).
+Round numbers to desired precision.
+Format numeric values for better readability.
+Aggregate and analyze numeric data efficiently.
+*/
